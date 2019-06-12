6e0b9a040e902fec74ca0b6b70b0dc36da61b320
ruei wang <14790379688@163.com>
2019-06-12 19:25:26 +0800

    提交了一个gitproject项目

---
+++ src/com/zerovr/gittest/gitproject.java
@@ -0,0 +1,13 @@
+package com.zerovr.gittest;
+
+public class gitproject {
+    public static void main(String[] args) {
+        System.out.println("第一个git测试");
+        System.out.println("第一个git测试");
+        System.out.println("第一个git测试");
+        System.out.println("第一个git测试");
+        System.out.println("第一个git测试");
+        System.out.println("第一个git测试");
+        System.out.println("第一个git测试");
+    }
+}
